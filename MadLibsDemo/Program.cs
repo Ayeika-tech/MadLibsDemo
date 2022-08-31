8d3efd83f3e4d29c34c78d6308b769b4a0b4b058
Ayeika-tech <68619379+Ayeika-tech@users.noreply.github.com>
2022-08-31 09:06:25 -0500

    Add project files.

---
+++ MadLibsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, adjective, noun;
+
+            Console.Write("Enter a name ");
+            name = Console.ReadLine();
+            Console.Write("Enter an adjetive ");
+            adjective = Console.ReadLine();
+            Console.Write("Enter a noun ");
+            noun = Console.ReadLine();
+
+            Console.WriteLine("Please excuse " + name + " , who is ");
+            Console.WriteLine("far too " + adjective + " to attend " + noun + " class.");
+
+
+            Console.ReadLine();
+        }
+    }
+}
